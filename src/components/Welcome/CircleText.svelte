8fcb0fd21fb697792be3fa8930f1de9cfd95e5a9
Hex09AF <lsqhuy@gmail.com>
2023-02-08 19:58:26 +0700

    Basic gsap

---
+++ src/components/Welcome/CircleText.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let fillColor = 'fill-white';
+</script>
+
+<svg
+	width="100%"
+	height="100%"
+	viewBox="0 0 600 400"
+	version="1.1"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+>
+	<path
+		id="multiarc"
+		d="
+  M 120, 200
+  a 175,175 0 1,1 350,0
+  a 175,175 0 1,1 -350,0
+  "
+		fill="none"
+	/>
+	<text dy="1" font-size="40" textLength="1050" font-weight="bolder">
+		<textPath id="multiarcText" href="#multiarc" aria-label="Smashing" class={fillColor}>
+			SMASHING MAGAZINE SMASHING MAGAZINE
+		</textPath>
+	</text>
+</svg>
--- src/components/Welcome/Welcome.svelte
+++ src/components/Welcome/Welcome.svelte
@@ -3,39 +3,53 @@
 
 	import { gsap } from 'gsap';
 	import { ScrollTrigger } from 'gsap/ScrollTrigger';
+	import CircleText from './CircleText.svelte';
 
 	onMount(() => {
 		gsap.registerPlugin(ScrollTrigger);
 
-		const welcomes = gsap.utils.toArray('.welcome');
-		welcomes.forEach((welcome, i) => {
-			const tl = gsap.timeline();
-			tl.from(welcome, { xPercent: 100 * (i % 2 == 0 ? 1 : -1) }).to(welcome, {
-				yPercent: 500,
-				opacity: 0
-			});
-			ScrollTrigger.create({
-				animation: tl,
-				markers: true,
-				trigger: '#welcome-wrapper',
-				start: `-=${i} top`,
-				scrub: 1,
-				pinSpacing: false,
-				pin: true
-			});
-		});
-
-		const tl = gsap.timeline();
-		tl.to('#circle', { width: '100vw', height: '100vw', borderRadius: 0 }).from('#circle-2', {
-			x: '-100vw',
-			borderRadius: 9999
-		});
+		const tl = gsap
+			.timeline()
+			.from('#fs-welcome', { xPercent: -100 })
+			.from('#sc-welcome', { xPercent: 100 }, 0)
+			.from('#tr-welcome', { xPercent: -100 }, 0)
+			.to('#circle', { width: '100vw', height: '100vw', borderRadius: 0 }, 0)
+			.to('#fs-welcome', { yPercent: 250, opacity: 0 }, 0.5)
+			.to('#sc-welcome', { yPercent: 150, opacity: 0 }, 0.5)
+			.to('#tr-welcome', { yPercent: 100, opacity: 0 }, 0.5)
+			.to('#circle', { opacity: 0 }, 0.75)
+			.from(
+				'#circle-2',
+				{
+					x: '-100vw',
+					borderRadius: 9999
+				},
+				0.75
+			)
+			.from('#fs-name', { xPercent: -100 }, 0.8)
+			.from('#sc-name', { xPercent: -100 }, 0.9)
+			.from('#tr-name', { xPercent: -100 }, 1)
+			.to('#fs-name', { xPercent: 100, opacity: 0 }, 1.6)
+			.to('#sc-name', { xPercent: 100, opacity: 0 }, 1.55)
+			.to('#tr-name', { xPercent: 100, opacity: 0 }, 1.5)
+			.to('#circle-2', { opacity: 0 }, 1.5)
+			.to('#circle-3', { width: '100vw', height: '100vw', borderRadius: 0 }, 2)
+			.to('#fs-text', { opacity: 1, rotate: 50 }, 2)
+			.to('#sc-text', { opacity: 1, rotate: -70 }, 2)
+			.to('#tr-text', { opacity: 1, rotate: 90 }, 2)
+			.to('#qd-text', { opacity: 1, rotate: -110 }, 2)
+			.to('#fs-text', { opacity: 1, width: '3000px', height: '3000px', ease: 'back.in(1.7)' }, 2.5)
+			.to('#sc-text', { opacity: 1, width: '3000px', height: '3000px', ease: 'back.in(1.7)' }, 2.5)
+			.to('#tr-text', { opacity: 1, width: '3000px', height: '3000px', ease: 'back.in(1.7)' }, 2.5)
+			.to('#qd-text', { opacity: 1, width: '3000px', height: '3000px', ease: 'back.in(1.7)' }, 2.5)
+			.to('#circle-3', { width: '0vw', height: '0vw', borderRadius: 9999 }, 2.5);
 
 		ScrollTrigger.create({
 			animation: tl,
 			markers: true,
 			trigger: '#welcome-wrapper',
-			start: `-=5 top`,
+			start: `top top`,
+			end: '+=5000',
 			scrub: 1,
 			pin: true
 		});
@@ -57,17 +71,60 @@
 </script>
 
 <section class="z-20 uppercase font-bold leading-[1]">
-	<div id="welcome-wrapper" class="relative h-screen">
+	<div id="welcome-wrapper" class="relative h-[100vh]">
 		<div
 			id="circle"
 			class="bg-red-500 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full"
 		/>
 		<div
 			id="circle-2"
-			class="bg-indigo-600 w-[100vw] h-[100vw] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+			class="bg-indigo-500 w-[100vw] h-[100vw] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+		/>
+		<div
+			id="circle-3"
+			class="bg-amber-500 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full"
 		/>
 		<div id="fs-welcome" class="welcome text-[20vw] text-red-600">Welcome</div>
 		<div id="sc-welcome" class="mt-[-100px] welcome text-[20vw]">Welcome</div>
 		<div id="tr-welcome" class="mt-[-150px] welcome text-[20vw] text-zinc-800">Welcome</div>
+
+		<div class="absolute top-0">
+			<div id="fs-name" class="name text-[17vw] text-indigo-600">Quoc Huy</div>
+			<div id="sc-name" class="mt-[-100px] name text-[17vw]">Quoc Huy</div>
+			<div id="tr-name" class="mt-[-150px] name text-[17vw] text-zinc-800">Quoc Huy</div>
+		</div>
+
+		<div class="absolute top-0 w-full h-full">
+			<div class="relative w-full h-full">
+				<div
+					id="fs-text"
+					class="origin-center opacity-0 w-[25vw] h-[25vw] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+				>
+					<CircleText fillColor="fill-amber-600" />
+				</div>
+				<div
+					id="sc-text"
+					class="origin-center opacity-0 w-[55vw] h-[55vw] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+				>
+					<CircleText />
+				</div>
+				<div
+					id="tr-text"
+					class="origin-center opacity-0 w-[90vw] h-[90vw] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+				>
+					<CircleText fillColor="fill-amber-600" />
+				</div>
+				<div
+					id="qd-text"
+					class="origin-center opacity-0 w-[150vw] h-[150vw] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+				>
+					<CircleText />
+				</div>
+			</div>
+		</div>
+
+		<div>Quoc Hex Hex</div>
+		<div>Hex Hex Hex</div>
+		<div>Hex Hex Huy</div>
 	</div>
 </section>
